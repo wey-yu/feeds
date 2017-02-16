f7188c0f4c5a75a70799541d415f83f1f8573737
k33g <ph.charriere@gmail.com>
2017-02-16 12:27:14 +0100

    🚀 start working

---
+++ index.js
@@ -0,0 +1,19 @@
+// demo
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+let port = process.env.PORT || 8080;
+
+let app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(express.static('public'));
+
+app.get('/hello', (req, res) => {
+  res.send({message:"I 💙 🐼", remark:"hello 🌍"});
+})
+
+app.listen(port)
+console.log(`🌍 Web Server is started - listening on ${port}`)
